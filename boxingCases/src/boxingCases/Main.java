3c90a54821866cc9c3bd568e0d55137513d7349b
Gorkem <gorkemtore1@gmail.com>
2023-01-10 20:24:51 +0300

    boxingCases eklendi.

---
+++ boxingCases/src/boxingCases/Main.java
@@ -0,0 +1,35 @@
+package boxingCases;
+
+public class Main {
+	byte primitiveByte = 1;
+	Byte byteObject = primitiveByte;
+	byte byteValue = byteObject;
+
+	short primitiveShort = 1;
+	Short shortObject = primitiveShort;
+	short shortValue = shortObject;
+
+	int primitiveInt = 1;
+	Integer intObject = primitiveInt;
+	int intValue = intObject;
+
+	long primitiveLong = 1L;
+	Long longObject = primitiveLong;
+	long longValue = longObject;
+
+	float primitiveFloat = 1.0f;
+	Float floatObject = primitiveFloat;
+	float floatValue = floatObject;
+
+	double primitiveDouble = 1.0d;
+	Double doubleObject = primitiveDouble;
+	double doubleValue = doubleObject;
+
+	char primitiveChar = 'a';
+	Character charObject = primitiveChar;
+	char charValue = charObject;
+
+	boolean primitiveBoolean = true;
+	Boolean booleanObject = primitiveBoolean;
+	boolean booleanValue = booleanObject;
+}
