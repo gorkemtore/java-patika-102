c4a43ad154ad5baf0bc35266c472527793895e7c
Gorkem <gorkemtore1@gmail.com>
2023-01-17 18:50:49 +0300

    static-initializer silindi.

---
+++ genericListHW/src/genericListHW/List.java
@@ -0,0 +1,6 @@
+package genericListHW;
+
+public class List <T>{
+	
+	List<T> list1 = new List<>();
+}
---
+++ genericListHW/src/genericListHW/Main.java
@@ -0,0 +1,11 @@
+package genericListHW;
+
+public class Main {
+
+	public static void main (String[] args) {
+		
+		
+
+	}
+
+}
